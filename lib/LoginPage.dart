daecd5f1fe09793c35594c96a1ae6c3cadf61866
soumyabrataghosh <ghoshsoumyabrata70@gmail.com>
2026-08-28 21:29:00 +0530

    Today's class i had added AniatedContainer and it's property & concept

--- lib/LoginPage.dart
+++ lib/LoginPage.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
@@ -18,49 +17,16 @@ class LoginPage extends StatelessWidget {
       body: Center(
         child: Column(
           children: [
-          SizedBox(height: 60,),
-          Text("Login", style: TextStyle(fontSize: 28),),
-
-            Padding(
-              padding: const EdgeInsets.all(25),
-              child: TextField(
-                decoration: InputDecoration(
-                    focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                        borderSide: BorderSide(color: Colors.green, width: 3)
-                    ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(40),
-                  ),
-                 labelText: "Enter your gmail",
-                  suffixText: "@gmail.com"
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(25),
-              child: TextField(
-                enabled: true,
-                keyboardType: TextInputType.emailAddress,
-                obscureText: true,
-                obscuringCharacter: "π",
-                decoration: InputDecoration(
-                  disabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: BorderSide(color: Colors.blue, width: 5)
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: BorderSide(color: Colors.purple, width: 3)
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.orange, width: 3),
-                    borderRadius: BorderRadius.circular(40),
-                  ),
-                 labelText: "Enter your password",
-                ),
-              ),
-            )
+            SizedBox(height: 100,),
+             AnimatedContainer(
+               duration: Duration(milliseconds: 400),
+               width: 200,
+               height: 200,
+               decoration: BoxDecoration(
+                 borderRadius: BorderRadius.circular(10),
+                 color: Colors.blue
+               ),
+             ),
 
           ],
         ),
