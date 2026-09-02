dac61d9937a8110f59c94c68b74a6ffdb8e3201c
soumyabrataghosh <ghoshsoumyabrata70@gmail.com>
2026-09-02 22:47:16 +0530

    Today's class i have started to create this design , this part includes icon, heading and sub heading and linear background

--- lib/LoginPage.dart
+++ lib/LoginPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -12,84 +13,98 @@ class _LoginPageState extends State<LoginPage> {
   @override
   void initState() {
     super.initState();
-
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blue,
-        title: Text(
-          "What'sApp",
-          style: TextStyle(color: Colors.white, fontSize: 25),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            transform: GradientRotation(20),
+            colors: [Color(0xffF0F1D7), Color(0x00C99A00)],
+          ),
+        ),
+        child: Center(
+          child: Column(
+            children: [
+              // SizedBox(height: 100,),
+              //  AnimatedContainer(
+              //    duration: Duration(milliseconds: 400),
+              //    width: width,
+              //    height: height,
+              //    decoration: BoxDecoration(
+              //      borderRadius: BorderRadius.circular(10),
+              //      color: Colors.blue
+              //    ),
+              //  ),
+              // SizedBox(height: 100,),
+              //
+              // ElevatedButton(onPressed: (){
+              //   // backend code
+              //   setState(() {
+              //     //        false
+              //     width = width==250?100:250;
+              //     height = height==200?100:200;
+              //   });
+              //
+              // }, child: Text("Press me"))
+
+              // Expanded(
+              //   flex: 4,
+              //   child: Container(color: Colors.red,),
+              // ),
+              // Expanded(
+              //   child: Container(color: Colors.brown,),
+              // ),
+
+              // Container(
+              //   margin: EdgeInsets.all(40),
+              //   width: 300,
+              //   height: 200,
+              //   decoration: BoxDecoration(
+              //     borderRadius: BorderRadius.circular(40),
+              //     color: Colors.orange,
+              //     boxShadow: [
+              //       BoxShadow(color: Colors.blue, blurRadius: 20, spreadRadius: 15, offset: Offset(20, 0)),
+              //       BoxShadow(color: Colors.brown, blurRadius: 20, spreadRadius: 15, offset: Offset(-30, 0)),
+              //     ]
+              //   ),
+              // )
+              SizedBox(height: 60),
+
+              // Card(
+              //   elevation: 20,
+              //   shadowColor: Colors.blue,
+              //   color: Colors.orange,
+              //   child: SizedBox(
+              //     width: 200,
+              //     height: 150,
+              //
+              //   ),
+              // )
+              Image.asset("assets/icon.png", scale: 2),
+              SizedBox(height: 40),
+              Text(
+                "Welcome Back To Tasker",
+                style: GoogleFonts.roboto(
+                  fontSize: 28,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              SizedBox(height: 18,),
+              Text(
+                "Enter Valid User Name & password\n to continue",
+                textAlign: TextAlign.center,
+                style: GoogleFonts.roboto(
+                  fontSize: 15,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-
-      body: Center(
-        child: Column(
-          children: [
-            // SizedBox(height: 100,),
-            //  AnimatedContainer(
-            //    duration: Duration(milliseconds: 400),
-            //    width: width,
-            //    height: height,
-            //    decoration: BoxDecoration(
-            //      borderRadius: BorderRadius.circular(10),
-            //      color: Colors.blue
-            //    ),
-            //  ),
-            // SizedBox(height: 100,),
-            //
-            // ElevatedButton(onPressed: (){
-            //   // backend code
-            //   setState(() {
-            //     //        false
-            //     width = width==250?100:250;
-            //     height = height==200?100:200;
-            //   });
-            //
-            // }, child: Text("Press me"))
-
-            // Expanded(
-            //   flex: 4,
-            //   child: Container(color: Colors.red,),
-            // ),
-            // Expanded(
-            //   child: Container(color: Colors.brown,),
-            // ),
-
-            // Container(
-            //   margin: EdgeInsets.all(40),
-            //   width: 300,
-            //   height: 200,
-            //   decoration: BoxDecoration(
-            //     borderRadius: BorderRadius.circular(40),
-            //     color: Colors.orange,
-            //     boxShadow: [
-            //       BoxShadow(color: Colors.blue, blurRadius: 20, spreadRadius: 15, offset: Offset(20, 0)),
-            //       BoxShadow(color: Colors.brown, blurRadius: 20, spreadRadius: 15, offset: Offset(-30, 0)),
-            //     ]
-            //   ),
-            // )
-
-            SizedBox(height: 30,),
-            Card(
-              elevation: 20,
-              shadowColor: Colors.blue,
-              color: Colors.orange,
-              child: SizedBox(
-                width: 200,
-                height: 150,
-
-              ),
-            )
-
-
-          ],
-        ),
-      )
-
     );
   }
 }
