5dcf5dc62bf80d083b74055b51806aa4527075b9
Anubhab <anubhab0208@gmail.com>
2026-09-17 21:24:01 +0530

    today’s class Sir have implemented the login page circular indicator machanism with toggle function with circularprogressIndicator

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -9,22 +9,57 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  double width = 100;
+  bool isPressed = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
       body:  AnimateGradient(
-        primaryColors: const [
-          Colors.blue,
-          Colors.pinkAccent,
-          Colors.white,
-        ],
-        secondaryColors: const [
-          Colors.black,
-          Colors.blueAccent,
-          Colors.white,
-        ],
-        child: Container(),
+          primaryColors: const [
+            Colors.blue,
+            Colors.pinkAccent,
+            Colors.white,
+          ],
+          secondaryColors: const [
+            Colors.black,
+            Colors.blueAccent,
+            Colors.white,
+          ],
+          child: Center(
+            child: Column(
+              children: [
+                SizedBox(height: 100,),
+
+                InkWell(
+                 onTap: (){
+                   setState(() {
+                     if(isPressed==false){
+                       width = 40;
+                       isPressed = true;
+                     }else{
+                       width = 100;
+                       isPressed = false;
+                     }
+                   });
+
+                 },
+                  child: AnimatedContainer(
+                    duration: Duration(milliseconds: 300),
+                    width: width,
+                    height: 40,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: isPressed?Colors.transparent:Colors.blue
+                    ),
+                    child: Center(child: isPressed?CircularProgressIndicator(color: Colors.white,):Text("Login")),
+                  ),
+                ),
+
+
+              ],
+            ),
+          )
       ),
     );
   }
