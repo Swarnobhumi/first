a597f5cc8a1dc8e8319b0e52f3dae4d76c6f25ea
Anubhab <anubhab0208@gmail.com>
2026-09-17 21:34:54 +0530

    implementation of animatedSwitcher

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -15,51 +15,62 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:  AnimateGradient(
-          primaryColors: const [
-            Colors.blue,
-            Colors.pinkAccent,
-            Colors.white,
-          ],
-          secondaryColors: const [
-            Colors.black,
-            Colors.blueAccent,
-            Colors.white,
-          ],
-          child: Center(
-            child: Column(
-              children: [
-                SizedBox(height: 100,),
+      body: AnimateGradient(
+        primaryColors: const [Colors.blue, Colors.pinkAccent, Colors.white],
+        secondaryColors: const [Colors.black, Colors.blueAccent, Colors.white],
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(height: 100),
 
-                InkWell(
-                 onTap: (){
-                   setState(() {
-                     if(isPressed==false){
-                       width = 40;
-                       isPressed = true;
-                     }else{
-                       width = 100;
-                       isPressed = false;
-                     }
-                   });
+              // InkWell(
+              //  onTap: (){
+              //    setState(() {
+              //      if(isPressed==false){
+              //        width = 40;
+              //        isPressed = true;
+              //      }else{
+              //        width = 100;
+              //        isPressed = false;
+              //      }
+              //    });
+              //
+              //  },
+              //   child: AnimatedContainer(
+              //     duration: Duration(milliseconds: 300),
+              //     width: width,
+              //     height: 40,
+              //     decoration: BoxDecoration(
+              //       borderRadius: BorderRadius.circular(10),
+              //       color: isPressed?Colors.transparent:Colors.blue
+              //     ),
+              //     child: Center(child: isPressed?CircularProgressIndicator(color: Colors.white,):Text("Login")),
+              //   ),
+              // ),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    isPressed == true ? isPressed = false : isPressed = true;
+                  });
+                },
+                child: Text("Change Widget"),
+              ),
 
-                 },
-                  child: AnimatedContainer(
-                    duration: Duration(milliseconds: 300),
-                    width: width,
-                    height: 40,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: isPressed?Colors.transparent:Colors.blue
-                    ),
-                    child: Center(child: isPressed?CircularProgressIndicator(color: Colors.white,):Text("Login")),
-                  ),
+              AnimatedSwitcher(
+                duration: Duration(milliseconds: 500),
+                child:isPressed
+                    ? SizedBox(width: 200, height: 200, child: Card(elevation: 4))
+                    : Container(
+                  width: 300,
+                  height: 300,
+                  decoration: BoxDecoration(color: Colors.blue),
                 ),
+              )
 
 
-              ],
-            ),
-          )
+            ],
+          ),
+        ),
       ),
     );
   }
