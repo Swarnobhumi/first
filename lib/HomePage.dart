8070d9309c8962fabf11839ba08d6d2653a10113
soumyabrataghosh <ghoshsoumyabrata70@gmail.com>
2026-09-19 20:57:39 +0530

    Today's class i have implemented Tween Animation and it's Feature
    Signed-off-by: soumyabrataghosh <ghoshsoumyabrata70@gmail.com>

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -9,22 +9,46 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  double currentValue = 50;
+
+  // Tween Animation
+  // Starting value & ending  value
+  // 1...............10
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: AnimateGradient(
+        primaryColors: const [Colors.blue, Colors.pinkAccent, Colors.white],
+        secondaryColors: const [Colors.black, Colors.blueAccent, Colors.white],
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(height: 100),
 
-      body:  AnimateGradient(
-        primaryColors: const [
-          Colors.blue,
-          Colors.pinkAccent,
-          Colors.white,
-        ],
-        secondaryColors: const [
-          Colors.black,
-          Colors.blueAccent,
-          Colors.white,
-        ],
-        child: Container(),
+              TweenAnimationBuilder(
+                  tween: Tween<double>(begin: 50, end: currentValue),
+                  duration: Duration(seconds: 1),
+                  builder: (context, value, child) {
+                    print(value);
+                     return InkWell(
+                       onTap: (){
+                         setState(() {
+                           currentValue = currentValue==50?250:50; //toggle function
+                         });
+                       },
+                       child: Container(
+                         width: value,
+                         height: value,
+                         color: Colors.orange,
+                       ),
+                     );
+                  },
+              )
+
+
+            ],
+          ),
+        ),
       ),
     );
   }
